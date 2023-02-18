4a572b7d5604c33283c7727a0c6d2c6bbce552f7
inakiesk <inakieskruiz@gmail.com>
2023-02-18 14:55:48 -0600

    Add Calculator buttons with styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CalculatorBtn from './CalculatorBtn';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -12,7 +13,40 @@ class Calculator extends React.Component {
     const { display } = this.state;
     return (
       <div className="calculator">
-        { display }
+        <div className="displayScreen">
+          {display}
+        </div>
+        <div className="calcBody">
+          <div className="calcRow">
+            <CalculatorBtn value="AC" styling="calcBtn" />
+            <CalculatorBtn value="+/-" styling="calcBtn" />
+            <CalculatorBtn value="%" styling="calcBtn" />
+            <CalculatorBtn value="÷" styling="calcBtn operator" />
+          </div>
+          <div className="calcRow">
+            <CalculatorBtn value="7" styling="calcBtn" />
+            <CalculatorBtn value="8" styling="calcBtn" />
+            <CalculatorBtn value="9" styling="calcBtn" />
+            <CalculatorBtn value="x" styling="calcBtn operator" />
+          </div>
+          <div className="calcRow">
+            <CalculatorBtn value="4" styling="calcBtn" />
+            <CalculatorBtn value="5" styling="calcBtn" />
+            <CalculatorBtn value="6" styling="calcBtn" />
+            <CalculatorBtn value="-" styling="calcBtn operator" />
+          </div>
+          <div className="calcRow">
+            <CalculatorBtn value="1" styling="calcBtn" />
+            <CalculatorBtn value="2" styling="calcBtn" />
+            <CalculatorBtn value="3" styling="calcBtn" />
+            <CalculatorBtn value="+" styling="calcBtn operator" />
+          </div>
+          <div className="calcRow">
+            <CalculatorBtn value="0" styling="btn0" />
+            <CalculatorBtn value="." styling="calcBtn" />
+            <CalculatorBtn value="=" styling="calcBtn operator" />
+          </div>
+        </div>
       </div>
     );
   }
