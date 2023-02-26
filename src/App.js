f739e6899f917725c7b8835adb9f55dd00038d5a
inakiesk <inakieskruiz@gmail.com>
2023-02-25 19:22:14 -0600

    Add routes to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Calculator" element={<Calculator />} />
+        <Route path="Quote" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
